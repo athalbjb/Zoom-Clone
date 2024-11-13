b5db4e4f92068634422dead3c8d3589f10e6e724
xHacker <115514882+athalbjb@users.noreply.github.com>
2024-11-13 03:49:53 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,43 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from "@/components/ui/toaster";
-import "@stream-io/video-react-sdk/dist/css/styles.css";
-import "react-datepicker/dist/react-datepicker.css";
-import NextTopLoader from "nextjs-toploader";
+import { Toaster } from '@/components/ui/toaster';
+import { ClerkProvider } from '@clerk/nextjs';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ReactNode } from 'react';
 
-const inter = Inter({ subsets: ["latin"] });
+import '@stream-io/video-react-sdk/dist/css/styles.css';
+import 'react-datepicker/dist/react-datepicker.css';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "ZOOM",
-  description: "Video Calling App",
+  title: 'YOOM',
+  description: 'Video calling App',
   icons: {
-    icon: "/icons/logo.svg",
+    icon: '/icons/logo.svg',
   },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="en">
-      <ClerkProvider>
+      <ClerkProvider
+        appearance={{
+          layout: {
+            socialButtonsVariant: 'iconButton',
+            logoImageUrl: '/icons/yoom-logo.svg',
+          },
+          variables: {
+            colorText: '#fff',
+            colorPrimary: '#0E78F9',
+            colorBackground: '#1C1F2E',
+            colorInputBackground: '#252A41',
+            colorInputText: '#fff',
+          },
+        }}>
         <body className={`${inter.className} bg-dark-2`}>
-          <NextTopLoader />
-          {children}
           <Toaster />
+          {children}
         </body>
       </ClerkProvider>
     </html>
